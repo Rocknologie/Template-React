e9296133e2139794c64b080739f942555f8fce5e
Rocknologie <rocknologie@hotmail.fr>
2020-11-29 21:25:40 +0100

    First commit

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * Component's application.
+ * 
+ * In this component we make all root.
+ */
+export const App: React.FC = () => (
+  <></>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { App } from './components/App';
+import './tailwind.output.css';
 import reportWebVitals from './reportWebVitals';
+import { start } from 'repl';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
